c3ad60e308176ee6c944809203c0312f161f8d7f
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-02-05 13:07:04 +0100

    Remove sand on click

---
+++ Assets/Script/Tile Script/ClickOnTileToRemoveSand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FloodedVillage {
+    [RequireComponent(typeof(Tile))]
+    public class ClickOnTileToRemoveSand : MonoBehaviour
+    {
+        private void Awake()
+        {
+            _tile = GetComponent<Tile>();
+        }
+
+        private void OnMouseDown()
+        {
+            if (_tile.type == TileType.sand)
+            {
+                _tile.type = TileType.empty;
+            }
+        }
+
+        private Tile _tile;
+    }
+}
